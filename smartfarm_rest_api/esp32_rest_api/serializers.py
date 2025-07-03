1d644f3daa0d576002ec80414770b070a10f140e
KINOX <kinox1010@gmail.com>
2025-07-03 17:10:32 +0900

    Delete smartfarm_rest_api directory

--- smartfarm_rest_api/esp32_rest_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SensorData
-
-class SensorDataSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model  = SensorData
-        fields = "__all__"  # 모든 필드 자동 직렬화
